4cd6207dcf6b718d003c51572691d631c98cf61a
유진 신 <raki2001@o365.yonsei.ac.kr>
2021-02-04 23:22:18 +0900

    Added LightmapSizeSetter

---
+++ Assets/Scripts/LightmapSizeSetter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    //[ExecuteInEditMode]
+    public class LightmapSizeSetter : MonoBehaviour
+    {
+        [SerializeField]
+        private int _lightmatScale = 128;
+
+        private void Start()
+        {
+            var renderers = GetComponentsInChildren<MeshRenderer>();
+
+            foreach (var renderer in renderers)
+            {
+                //renderer.scaleInLightmap = _lightmatScale;
+            }
+        }
+    }
+}
