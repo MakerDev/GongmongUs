e85877c67998593c4a7e2cb4abe0a8062d8ad3cd
yujinshin <raki2001@o365.yonsei.ac.kr>
2021-02-07 12:07:04 +0900

    Outline Effect bug is fixed.

---
+++ Assets/Scripts/OutlineEffectAttacher.cs
@@ -0,0 +1,24 @@
+﻿using cakeslice;
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineEffectAttacher : NetworkBehaviour
+{
+    [SerializeField]
+    private GameObject _cameraObject;
+
+    private void Start()
+    { 
+        if (isLocalPlayer)
+        {
+            var outlineEffect = _cameraObject.AddComponent<OutlineEffect>();
+            outlineEffect.lineColor0 = new Color(242, 255, 0);
+            outlineEffect.fillColor = new Color(252, 182, 0);
+            outlineEffect.lineThickness = 2.3f;
+            outlineEffect.lineIntensity = 1.6f;
+            outlineEffect.useFillColor = true;
+        }
+    }
+}
