3e839e837a87e45630fcdde59dbed677dce7b0d4
yujinshin <raki2001@o365.yonsei.ac.kr>
2021-02-21 01:41:49 +0900

    Added Compress mission.

---
+++ Assets/Scripts/MiniGames/CompressMissionMiniGame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompressMissionMiniGame : MiniGame
+{
+    private const int TOGGLES_COUNT = 6;
+
+    [SerializeField]
+    private Toggle[] _toggles = new Toggle[TOGGLES_COUNT];
+    private bool[] _answers = new bool[TOGGLES_COUNT] { true, false, false, false, true, false };
+
+    public override void ResetGame()
+    {
+        foreach (var toggle in _toggles)
+        {
+            toggle.isOn = false;
+        }
+    }
+
+    public void Cancle()
+    {
+        CancelMiniGame();
+    }
+
+    public void Submit()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            if (_toggles[i].isOn != _answers[i])
+            {
+                Debug.Log("Fail");
+                Cancle();
+                return;
+            }
+        }
+
+        MiniGameResult.Passed = true;
+        CompleteMiniGame(MiniGameResult);
+    }
+}
